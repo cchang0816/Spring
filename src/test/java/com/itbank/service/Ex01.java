c1f7640819d190041c0c72ca3e0cf0f9ebfbf8ec
cchang0816 <ckddyd0816@gmail.com>
2023-11-24 12:58:05 +0900

    Junit clear

---
+++ src/test/java/com/itbank/service/Ex01.java
@@ -0,0 +1,23 @@
+package com.itbank.service;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+// junit만 있어도 기본적인 테스트는 가능
+public class Ex01 {
+	
+	@Test
+	public void run1() {
+		System.out.println("junit 테스트~");
+	}
+	
+	@Test 
+	public void run2() {
+		assertFalse(10 > 5);
+		// - false이면 test 성공으로 간주
+		// - true면 test 실패로 간주
+		
+		System.out.println("@Test는 딱히 순서대로 실행되진 않음");
+	}
+}
